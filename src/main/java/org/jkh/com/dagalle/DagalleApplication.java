712040e9e35e8fee90bfdc57d171d6817a4d88b4
newSun00 <169756578+NewJKH@users.noreply.github.com>
2026-03-03 18:07:55 +0900

    feat(project-setup): initialize new Spring Boot project with Gradle

---
+++ src/main/java/org/jkh/com/dagalle/DagalleApplication.java
@@ -0,0 +1,13 @@
+package org.jkh.com.dagalle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagalleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DagalleApplication.class, args);
+    }
+
+}
